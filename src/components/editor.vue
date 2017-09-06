bc17aaaef7f60ac82f6407193af80a5f6d5d04d0
dj940212 <2902273280@qq.com>
2017-09-06 22:05:01 +0800

    引入marked

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -1,116 +1,68 @@
 <template>
-    <div class="editor">
-        <div id="in" class="in">
-            <form>
-                <textarea  id="code" cols="100" rows="30">
-# (GitHub-Flavored) Markdown Editor
-
-Basic useful feature list:
-
-* Ctrl+S / Cmd+S to save the file
-* Ctrl+Shift+S / Cmd+Shift+S to choose to save as Markdown or HTML
-* Drag and drop a file into here to load it
-* File contents are saved in the URL so you can share files
-
-```javascript
-$(function(){
-  $('div').html('I am a div.');
-});
-```                    
-                </textarea>
-            </form>
-        </div>
-        <div id="out">
-            
-        </div>
+    <div id="editor" class="editor">
+        <textarea id="content" v-model="article" name="name" rows="20" cols="80" el='content'>
+## 海贼王
+###### 东动画
+        </textarea>
+        <div id="out" class="out" ref="out"></div>
     </div>
 </template>
-<script>
-import markdownIt from 'markdown-it'
-import hljs from 'highlight.js'
-import CodeMirror from 'codemirror'
 
+<script>
+import marked from 'marked'
+import highlight from 'highlight'
 export default {
-    mounted(){
-        const result = new markdownIt().render('# markdown-it!')
-        console.log(result)
-        this.init()
-        console.log(this.md.render)
-        this.editor()
+    data() {
+        return {
+            article : "",
+            endAticle: null
+        }
+    },
+    mounted() {
+    	this.init()
+        console.log(this)
+        // document.getElementById('editor').innerHTML =
+        //   marked('# Marked in browser\n\nRendered by *marked*.');
     },
     methods: {
-        data() {
-            return {
-            }
-        },
-        init() {
-            this.md = markdownIt({
-                html: true,
-                highlight: function (str, lang) {
-                    if (lang && hljs.getLanguage(lang)) {
-                        try {
-                            return hljs.highlight(lang, str).value;
-                        } catch (e) {}
-                    }
-                    return ''; // use external default escaping
-                }
-            })
-        },
-        update(e) {
-            console.log(e)
-            this.setOutput(e.getValue())
-            clearTimeout(this.hashto)
-            this.hashto = setTimeout(updateHash, 1000)
-        },
-        setOutput(val) {
-            val = val.replace(/<equation>((.*?\n)*?.*?)<\/equation>/ig, (a, b) => {
-                return '<img src="http://latex.codecogs.com/png.latex?' + encodeURIComponent(b) + '" />'
-            })
-
-            var out = document.getElementById('out');
-            var old = out.cloneNode(true);
-            out.innerHTML = this.md.render(val);
-            // emojify.run(out);
-            console.log("outHTML",out.innerHTML)
-
-              var allold = old.getElementsByTagName("*");
-              if (allold === undefined) return;
-
-              var allnew = out.getElementsByTagName("*");
-              if (allnew === undefined) return;
-
-              for (var i = 0, max = Math.min(allold.length, allnew.length); i < max; i++) {
-                if (!allold[i].isEqualNode(allnew[i])) {
-                  out.scrollTop = allnew[i].offsetTop;
-                  return;
-                }
-              }
-        },
-        editor() {
-            this.editorLive = CodeMirror.fromTextArea(document.getElementById('code'), {
-              mode: 'gfm',
-              lineNumbers: false,
-              matchBrackets: true,
-              lineWrapping: true,
-              theme: 'base16-light',
-              extraKeys: {"Enter": "newlineAndIndentContinueMarkdownList"}
-            });
-
-            this.editorLive.on('change', this.update);
-        },
-        updateHash(){
-          window.location.hash = btoa( // base64 so url-safe
-            RawDeflate.deflate( // gzip
-              unescape(encodeURIComponent( // convert to utf8
-                this.editor.getValue()
-              ))
-            )
-          );
+    	init() {
+			marked.setOptions({
+			  	renderer: new marked.Renderer(),
+			  	gfm: true,
+			  	tables: true,
+			  	breaks: false,
+			  	pedantic: false,
+			  	sanitize: false,
+			  	smartLists: true,
+			  	smartypants: false,
+			  	highlight: function (code) {
+				    return highlight.highlightAuto(code).value;
+				}
+			});
+    	}
+    },
+    computed: {
+        Marked: () => {
+            return marked(this.article)
+        }
+    },
+    watch: {
+        article: function(val) {
+            this.endAticle= marked(val)
+            this.$refs.out.innerHTML = this.endAticle
         }
     }
 }
 </script>
 
-<style lang="less" scoped> 
-
+<style lang="less" scoped="">
+    #content　{
+        float: left;
+    }
+    .out {
+        width: 40%;
+        height: 300px;
+        border: 1px solid #333;
+        float: right;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
 import Editor from '@/components/editor'
-import newEditor from '@/components/newEditor'
 
 Vue.use(Router)
 
@@ -10,8 +9,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'newEditor',
-      component: newEditor
+      name: 'Editor',
+      component: Editor
     }
   ]
 })
