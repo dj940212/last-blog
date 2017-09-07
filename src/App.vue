38b4e5ec1f58d3845a6085e645376d9c0e46ca2b
dj940212 <2902273280@qq.com>
2017-09-07 22:11:23 +0800

    修改header

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+      font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   /*text-align: center;*/
--- src/components/myheader.vue
+++ src/components/myheader.vue
@@ -1,21 +1,24 @@
 <template>
     <header class="header">
-        <div class="header-innner">
-            <div class="site-brand">
-                <a href="" class="brand">
-                    <span class="line-before"></span>
-                    <span class="title">业精于勤</span>
-                    <span class="line-after"></span>
-                </a>
+        <div class="header-inner">
+            <div class="logo-box">
+                <span class="logo"></span>
             </div>
-            <div class="nav">
-                <nav class="menu">
-                    <li class="memu-item">首页</li>
-                    <li class="memu-item">归档</li>
-                    <li class="memu-item">关于</li>
-                    <li class="memu-item">搜索</li>
-                    <li class="memu-item"><router-link to="/article/write">写博客</router-link></li>
-                </nav>
+            <div class="search-box">
+                <input type="text" name="" value="" placeholder="Search GitHub">
+            </div>
+            <div class="header-menu">
+                <ul>
+                    <li class="menu-item">Pull requests</li>
+                    <li class="menu-item">Issues</li>
+                    <li class="menu-item">Marketplace</li>
+                    <li class="menu-item">Explore</li>
+                </ul>
+            </div>
+            <div class="icon-box">
+                <div class="messge"></div>
+                <div class="add"></div>
+                <div class="avatar"></div>
             </div>
         </div>
     </header>
@@ -29,52 +32,57 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+    @import "../common/less/variable.less";
     .header {
-        box-shadow: 0 0 5px rgba(0,0,0,0.2);
-        .header-innner {
-            height: 48px;
-            max-width: 800px;
-            padding: 2px 0 1px;
-            position: relative;
-            margin:0 auto;
-            .nav {
-                .menu {
-                    float: right;
-                    // margin: 8px 0 0 0;
-                    text-align: center;
-                    .memu-item {
-                        width: 100px;
-                        color: #555;
-                        display: inline-block;
-                        height: 48px;
-                        line-height: 48px;
-                        text-align: center;
+        background: @header-bgcolor;
+        .header-inner {
+            padding: 11px 0;
+            max-width: @content-width;
+            margin: 0 auto;
+            display: flex;
+            align-items: center;
+            .logo-box{
+                margin-right: 15px;
+                .logo {
+                    width: 32px;
+                    height: 32px;
+                    display: block;
+                    background: url(http://icons.iconarchive.com/icons/danleech/simple/512/github-icon.png);
+                    background-size: contain;
+                    border-radius: 16px;
+                }
+            }
+            .search-box {
+                input {
+                    height: 30px;
+                    width: 300px;
+                    background-color: #3f4448;
+                    border: 0;
+                    border-radius: 3px;
+                    padding: 0 8px;
+                    color: rgba(255, 255, 255, 0.75);
+                    line-height: 18px;
+                    // font-size: 16px;
+                    &:focus {
+                        outline: none;
                     }
                 }
             }
-            .site-brand {
-                float: left;
-                margin-top: 4px;
-                margin-left: 10px;
-                .brand {
-                    height: 48px;
-                    color: #222;
-                    line-height: 34px;
-                    .title {
-                        font-size: 20px;
-                        font-weight: bolder;
-                        letter-spacing: 2px;
-                        font-family: "Microsoft YaHei";
-                    }
-                    .line-before, .line-after {
-                        display: block;
-                        margin: 0 auto;
-                        width: 75%;
-                        height: 2px;
-                        background-color: #222;
+            .header-menu {
+                ul {
+                    display: flex;
+                    margin: 0;
+                    padding: 0;
+                    .menu-item {
+                        list-style: none;
+                        margin-left: 16px;
+                        color: rgba(255, 255, 255, 0.75);
+                        font-size: 14px;
+                        font-weight: 600;
                     }
                 }
             }
         }
+
     }
 </style>
