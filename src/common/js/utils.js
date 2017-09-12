e15f02960f7173ce68bd5e60e1143d3ba6d6d68c
dj940212 <2902273280@qq.com>
2017-09-12 17:28:04 +0800

    9.12

---
+++ src/common/js/utils.js
@@ -0,0 +1,15 @@
+function formatNumber(n) {
+  n = n.toString()
+  return n[1] ? n : '0' + n
+}
+export const formatTime = (date) => {
+  var year = date.getFullYear()
+  var month = date.getMonth() + 1
+  var day = date.getDate()
+
+  var hour = date.getHours()
+  var minute = date.getMinutes()
+  var second = date.getSeconds();
+
+  return [year, month, day].map(formatNumber).join('-')
+}
--- src/components/activityMap.vue
+++ src/components/activityMap.vue
@@ -1,16 +1,47 @@
 <template>
 	<div class="map">
 		<div class="des">513 contributions in the last year</div>
+
 		<div class="map-inner">
 			<div class="block">
-				<div class="item" v-for="i in 368"></div>
+				<div class="item" v-for="i in dayNum" @click="calDate(i)"></div>
 			</div>
 		</div>
 	</div>
 </template>
 <script>
+import axios from 'axios'
+import {formatTime} from '@/common/js/utils'
 export default {
-	name: 'activityMap'
+	name: 'activityMap',
+	mounted() {
+		this.calDayNum()
+		this.getActivity()
+	},
+	data() {
+		return {
+			dayNum: 364,
+			activity:[]
+		}
+	},
+	methods: {
+		calDayNum() {
+			let day = new Date().getDay()
+			this.dayNum = day + 364
+		},
+		async getActivity() {
+			let res = await axios.get('http://localhost:3000/api/activity/all',{params:{
+				limit: this.dayNum
+			}})
+			this.activity = res.data.data
+		},
+		async calDate(index) {
+			let indexTime = new Date().getTime() - (this.dayNum - index)*24*60*60*1000
+			let indexDate = new Date(indexTime)
+			let dateStr = `${indexDate.getFullYear()}-${indexDate.getMonth()+1}-${indexDate.getDate()}`
+			console.log(dateStr)
+		}
+	}
 }
 </script>
 <style lang="less">
@@ -21,7 +52,15 @@ export default {
 			height: 170px;
 			border: 1px solid @border-color;
 			border-radius: 3px;
-			margin: 20px auto;
+			margin: 0 auto;
+			display: flex;
+			.month {
+				width: 901px;
+				height: 17px;
+				.month-item {
+
+				}
+			}
 			.block {
 				display: flex;
 				flex-direction: column;
@@ -41,6 +80,7 @@ export default {
 				}
 			}
 		}
+		
 	}
 	
 </style>
