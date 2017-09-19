65192804f5d041ab2dec65c941d05d261ca96cb9
dj940212 <2902273280@qq.com>
2017-09-19 15:50:20 +0800

    add ecosystem.json

---
+++ prod.server.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+var port = 8085;
+
+var app = express();
+
+var router = express.Router();
+
+router.get('/', function (req, res, next) {
+    req.url = './dist/index.html';
+    next();
+});
+
+app.use(router);
+
+app.use(express.static('./dist/static'));
+
+module.exports = app.listen(port, function (err) {
+    if (err) {
+        console.log(err);
+        return
+    }
+    console.log('Listening at http://localhost:' + port + '\n')
+});
