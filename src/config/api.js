8126cba497347bb82b2493f37b974e52938cc93c
dj940212 <2902273280@qq.com>
2017-09-20 17:25:00 +0800

    ...

--- src/config/api.js
+++ src/config/api.js
@@ -1,27 +1,27 @@
-// export default {
-//     // article
-//     'addArticleUrl': 'http://localhost:3000/api/article/save',
-//     'articleListUrl': 'http://localhost:3000/api/article/list',
-//     'articleUpdateUrl': 'http://localhost:3000/api/article/update',
-//     'articleDeleteUrl': 'http://localhost:3000/api/article/delete',
-//     'readArticleUrl': 'http://localhost:3000/api/article/read',
-
-//     // activity
-//     'getAllActivityUrl': 'http://localhost:3000/api/activity/all',
-//     'getOneDayActivityUrl': 'http://localhost:3000/api/activity/oneDay'
-
-// }
-
 export default {
     // article
-    'addArticleUrl': 'http://59.110.164.55:8086/api/article/save',
-    'articleListUrl': 'http://59.110.164.55:8086/api/article/list',
-    'articleUpdateUrl': 'http://59.110.164.55:8086/api/article/update',
-    'articleDeleteUrl': 'http://59.110.164.55:8086/api/article/delete',
-    'readArticleUrl': 'http://59.110.164.55:8086/api/article/read',
+    'addArticleUrl': 'http://localhost:8086/api/article/save',
+    'articleListUrl': 'http://localhost:8086/api/article/list',
+    'articleUpdateUrl': 'http://localhost:8086/api/article/update',
+    'articleDeleteUrl': 'http://localhost:8086/api/article/delete',
+    'readArticleUrl': 'http://localhost:8086/api/article/read',
 
     // activity
-    'getAllActivityUrl': 'http://59.110.164.55:8086/api/activity/all',
-    'getOneDayActivityUrl': 'http://59.110.164.55:8086/api/activity/oneDay'
+    'getAllActivityUrl': 'http://localhost:3000/api/activity/all',
+    'getOneDayActivityUrl': 'http://localhost:3000/api/activity/oneDay'
 
 }
+
+// export default {
+//     // article
+//     'addArticleUrl': 'http://59.110.164.55:8086/api/article/save',
+//     'articleListUrl': 'http://59.110.164.55:8086/api/article/list',
+//     'articleUpdateUrl': 'http://59.110.164.55:8086/api/article/update',
+//     'articleDeleteUrl': 'http://59.110.164.55:8086/api/article/delete',
+//     'readArticleUrl': 'http://59.110.164.55:8086/api/article/read',
+
+//     // activity
+//     'getAllActivityUrl': 'http://59.110.164.55:8086/api/activity/all',
+//     'getOneDayActivityUrl': 'http://59.110.164.55:8086/api/activity/oneDay'
+
+// }
